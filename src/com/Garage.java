e3d11c8a7d2715df9233121334fe437d20ccac55
Will Green <willgreen@Wills-MacBook-Pro.local>
2021-05-20 16:36:12 -0500

    WG: progress

--- src/com/Garage.java
+++ src/com/Garage.java
@@ -1,8 +1,12 @@
 package com;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Garage {
 	
 	protected Boolean full;
+	protected List<Object> vehicleList = new ArrayList<Object>();
 	
 	public Garage(Boolean full) {
 		
@@ -14,12 +18,21 @@ public class Garage {
 		
 	}
 	
-	public void addVehicle() {
+	public void addVehicle(Object vehicle) {
+		vehicleList.add(0, vehicle);
 		
 	}
 	
-	public void removeVehicle() {
+	public void removeVehicle(int vehicleId) {
 		
+		for(int i = 0; i < vehicleList.size(); i++) {
+			
+			Object vehicle = vehicleList.get(i);
+			
+			if(vehicle.vehicleId.equals(vehicleId)) {
+				vehicleList.remove(i);
+			}
+		}
 	}
 	
 	public void empty() {
