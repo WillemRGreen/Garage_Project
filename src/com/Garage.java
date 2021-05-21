a03155a9b22e2321555353623a4912ffdc836d1b
Will Green <willgreen@Wills-MacBook-Pro.local>
2021-05-21 11:35:52 -0500

    WG: final before solution

--- src/com/Garage.java
+++ src/com/Garage.java
@@ -6,39 +6,39 @@ import java.util.List;
 public class Garage {
 	
 	protected Boolean full = false;
-	protected List<Object> vehicleList = new ArrayList<Object>();
+	protected List<Vehicle> vehicleList = new ArrayList<Vehicle>();
 	
 	public Garage(Boolean full) {
 		this.full = full;
 	}
 	
-	public int billCalc() {
-		
-		int totalBill = 0;
-		for(int i = 0; i < vehicleList.size(); i++) {
-			if(Vehicle.getVehicleType().equals("bicycle")){
-				totalBill += 500;
-			} else if(Vehicle.getVehicleType().equals("motorcycle")) {
-				totalBill += 5000;
-			} else if(Vehicle.getVehicleType().equals("car")) {
-				totalBill += 10000;
-			}
-		}
-		return totalBill;
-	}
+//	public int billCalc() {
+//		
+//		int totalBill = 0;
+//		for(int i = 0; i < vehicleList.size(); i++) {
+//			if(Vehicle.getVehicleType().equals("bicycle")){
+//				totalBill += 500;
+//			} else if(Vehicle.getVehicleType().equals("motorcycle")) {
+//				totalBill += 5000;
+//			} else if(Vehicle.getVehicleType().equals("car")) {
+//				totalBill += 10000;
+//			}
+//		}
+//		return totalBill;
+//	}
 	
-	public void addVehicle(Object vehicle) {
+	public void addVehicle(Vehicle vehicle) {
 		vehicleList.add(0, vehicle);
 		
 	}
 	
-	public void removeVehicle(int vehicleId) {
-		for(int i = 0; i < vehicleList.size(); i++) {
-			if(Vehicle.getId() == vehicleId) {
-				vehicleList.remove(i);
-			}
-		}
-	}
+//	public void removeVehicle(int vehicleId) {
+//		for(int i = 0; i < vehicleList.size(); i++) {
+//			if(Vehicle.getId() == vehicleId) {
+//				vehicleList.remove(i);
+//			}
+//		}
+//	}
 	
 	public void empty() {
 		for(int i = 0; i < vehicleList.size(); i++) {
